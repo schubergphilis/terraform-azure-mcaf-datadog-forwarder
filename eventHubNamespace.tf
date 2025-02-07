c678441bbfd0a74d1e676cf23897175e73da0d90
rwiankowski <rwiankowski@mac.com>
2025-02-07 14:47:45 +0100

    Fixed cmk_encryption_key reference

--- eventHubNamespace.tf
+++ eventHubNamespace.tf
@@ -27,7 +27,7 @@ resource "azurerm_eventhub_namespace" "this" {
 
 resource "azurerm_eventhub_namespace_customer_managed_key" "this" {
   eventhub_namespace_id             = azurerm_eventhub_namespace.this.id
-  key_vault_key_ids                 = [data.key.cmk_encryption_key.value]
+  key_vault_key_ids                 = [data.azurerm_key_vault_key.value]
   user_assigned_identity_id         = azurerm_user_assigned_identity.datadog_mid.id
   infrastructure_encryption_enabled = true
 }
