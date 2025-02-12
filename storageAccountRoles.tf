35f90eb204300cf9b48b91143972478cb894c1b9
Bogdan Grozoiu <bogdan@grozoiu.com>
2025-02-12 14:28:57 +0100

    dif privilege

--- storageAccountRoles.tf
+++ storageAccountRoles.tf
@@ -8,7 +8,7 @@ resource "azurerm_role_assignment" "datadog_mid_sta_blob" {
 resource "azurerm_role_assignment" "datadog_mid_sta_file" {
   principal_id                     = azurerm_user_assigned_identity.datadog_mid.principal_id
   scope                            = data.azurerm_storage_account.this.id
-  role_definition_name             = "Storage File Data SMB Share Contributor"
+  role_definition_name             = "Storage File Data Privileged Contributor"
   skip_service_principal_aad_check = false
 }
 
