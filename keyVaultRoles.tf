9502bcf7214f571e410016be6812f53e9d7b703c
Bogdan Grozoiu <bogdan@grozoiu.com>
2025-02-20 11:31:25 +0100

    cleanup

--- keyVaultRoles.tf
+++
@@ -1,6 +0,0 @@
-#resource "azurerm_role_assignment" "principal" {
-#  principal_id                     = data.azurerm_client_config.current.object_id
-#  scope                            = data.azurerm_key_vault.this.id
-#  role_definition_name             = "Key Vault Crypto User"
-#  skip_service_principal_aad_check = false
-#}
