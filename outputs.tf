e06c03196740382d7a9e38a1e91e6e35172871b6
Bogdan Grozoiu <bogdan@grozoiu.com>
2025-02-20 10:25:40 +0100

    ehns resource id is used for the pep

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "storage_account_id" {
   value = var.storage_account != null ? module.storage_account.id : null
 }
 
+output "eventhub_namespace_id" {
+  value = azurerm_eventhub_namespace.this.id
+}
+
 output "function_app_id" {
   value = azurerm_linux_function_app.this.id
 }
