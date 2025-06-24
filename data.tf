98849634d09d7323d98caddc03ca87e088e84b16
Marius Waldhorst <mwaldhorst@schubergphilis.com>
2025-06-24 10:45:07 +0200

    Do not use datablock for resource created in module

--- data.tf
+++ data.tf
@@ -14,9 +14,3 @@ data "azurerm_key_vault_key" "cmk_encryption_key" {
   name         = var.key_vault_secret_cmk_key_name
   key_vault_id = data.azurerm_key_vault.this.id
 }
-
-data "azurerm_storage_account" "this" {
-  depends_on          = [module.storage_account]
-  name                = var.storage_account.name
-  resource_group_name = var.resource_group_name
-}
--- functionApp.tf
+++ functionApp.tf
@@ -12,28 +12,28 @@ resource "azurerm_user_assigned_identity" "func_datadog_mid" {
 
 resource "azurerm_role_assignment" "func_datadog_mid_sta_blob" {
   principal_id                     = azurerm_user_assigned_identity.func_datadog_mid.principal_id
-  scope                            = data.azurerm_storage_account.this.id
+  scope                            = module.storage_account.id
   role_definition_name             = "Storage Blob Data Contributor"
   skip_service_principal_aad_check = false
 }
 
 resource "azurerm_role_assignment" "func_datadog_mid_sta_file" {
   principal_id                     = azurerm_user_assigned_identity.func_datadog_mid.principal_id
-  scope                            = data.azurerm_storage_account.this.id
+  scope                            = module.storage_account.id
   role_definition_name             = "Storage File Data Privileged Contributor"
   skip_service_principal_aad_check = false
 }
 
 resource "azurerm_role_assignment" "func_datadog_mid_sta_queue" {
   principal_id                     = azurerm_user_assigned_identity.func_datadog_mid.principal_id
-  scope                            = data.azurerm_storage_account.this.id
+  scope                            = module.storage_account.id
   role_definition_name             = "Storage Queue Data Contributor"
   skip_service_principal_aad_check = false
 }
 
 resource "azurerm_role_assignment" "func_datadog_mid_sta_table" {
   principal_id                     = azurerm_user_assigned_identity.func_datadog_mid.principal_id
-  scope                            = data.azurerm_storage_account.this.id
+  scope                            = module.storage_account.id
   role_definition_name             = "Storage Table Data Contributor"
   skip_service_principal_aad_check = false
 }
